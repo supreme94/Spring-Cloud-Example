ad3eec7138405fec1034161705861eac63ef0770
liangpeng <183407401@qq.com>
2016-08-07 12:19:53 +0800

    ...

--- Config-Client/src/main/java/com/cloud/config/client/ConfigClientApplication.java
+++ Config-Client/src/main/java/com/cloud/config/client/ConfigClientApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConfigClientApplication {
 	@Value("${name.world}")
-	private String world;
+ String world;
 	
 	@RequestMapping("/")
     public String index(){
