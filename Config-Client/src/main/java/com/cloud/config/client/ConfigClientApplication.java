2557e885da03b13a1852aff343bf552c2d7f78fa
liangpeng <183407401@qq.com>
2016-08-08 16:22:02 +0800

    eureka server

--- Config-Client/src/main/java/com/cloud/config/client/ConfigClientApplication.java
+++ Config-Client/src/main/java/com/cloud/config/client/ConfigClientApplication.java
@@ -1,13 +1,15 @@
 package com.cloud.config.client;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 	@Value("${name.world}")
  String world;
@@ -18,7 +20,7 @@ public class ConfigClientApplication {
     }
 	
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
+		 new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
 	}
 
 }
